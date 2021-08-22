830edda03d4c96648442021e8f6df38b928cd250
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-08-21 23:43:40 -0500

    Remove need for specific commands

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -8,14 +8,12 @@ import {
 } from 'discord.js';
 import glob from 'glob';
 import { promisify } from 'util';
-import Command from '../interfaces/CommandStorage';
 import Event from '../interfaces/EventStorage';
 
 const globPromise = promisify(glob);
 
 export default class Bot extends Client {
   // readonly members
-  public readonly commands: Collection<string, Command> = new Collection();
   public readonly events: Collection<string, Event> = new Collection();
   public readonly logger: Consola = consola;
 
@@ -47,18 +45,6 @@ export default class Bot extends Client {
   }
 
   public start(): void {
-    // register commands
-    globPromise(`${__dirname}/../commands/**/*{.ts,.js}`)
-      .then((commands: string[]) => {
-        commands.map(async (commandPath: string) => {
-          import(commandPath).then((command: Command) => {
-            this.logger.info(`Registering command "${command.name}"...`);
-            this.commands.set(command.name, command);
-          });
-        });
-      })
-      .catch((e: unknown) => this.logger.error(e));
-
     // register events
     globPromise(`${__dirname}/../events/**/*{.ts,.js}`)
       .then((events: string[]) => {
--- src/interfaces/CommandStorage.ts
+++
@@ -1,6 +0,0 @@
-import Runner from './RunnerStorage';
-
-export default interface Command {
-  name: string;
-  run: Runner;
-}
