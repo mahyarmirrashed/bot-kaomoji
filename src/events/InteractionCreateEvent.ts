2e25753fa22e30b87b318e801c36d0fa2ad1bdd9
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-08-24 19:35:32 -0500

    Completed listener for interaction creation

---
+++ src/events/InteractionCreateEvent.ts
@@ -0,0 +1,28 @@
+import { hyperlink } from '@discordjs/builders';
+import { Interaction } from 'discord.js';
+import Bot from '../client/Client';
+import Runner from '../interfaces/RunnerStorage';
+
+export const run: Runner<Interaction> = async (
+  client: Bot,
+  interaction: Interaction,
+): Promise<void> => {
+  if (interaction.isCommand()) {
+    if (client.kaomojis.has(interaction.commandName)) {
+      interaction.reply(
+        `${
+          interaction.options.getString('message') || ''
+        } ${client.kaomojis.get(interaction.commandName)}`,
+      );
+    } else {
+      interaction.reply({
+        content: `Houston, we found a bug! Please report it ${hyperlink(
+          'here',
+          'https://github.com/mahyarmirrashed/bot-kaomoji/issues/new?assignees=&labels=&template=bug_report.md',
+        )} thanks!`,
+      });
+    }
+  }
+};
+
+export const name = 'interactionCreate';
