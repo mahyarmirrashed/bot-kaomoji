59b4746d905ef672cdb03c1927d799c4be3fcb3a
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-08-25 12:57:57 -0500

    Set bot rich presence on server

--- src/events/ReadyEvent.ts
+++ src/events/ReadyEvent.ts
@@ -1,9 +1,17 @@
+import { ActivityType, PresenceUpdateStatus } from 'discord-api-types/v9';
 import Bot from '../client/Client';
 import Runner from '../interfaces/RunnerStorage';
 
 export const run: Runner = async (client: Bot): Promise<void> => {
   if (client.user) {
+    // report sucessful login
     client.logger.success(`${client.user.username} successfully logged in!`);
+    // set bot rich presence on servers
+    client.user.setPresence({
+      status: PresenceUpdateStatus.Online,
+      afk: false,
+      activities: [{ name: '/joy', type: ActivityType.Listening }],
+    });
   } else {
     client.logger.error('`user` property on `client` was `null`...');
   }
