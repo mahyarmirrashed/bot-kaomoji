989e69d8c246d1e5261c152e70ea40227ee2ccdb
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-09-07 05:18:13 -0500

    feat: allow registration of variant kaomojis
    Using integer choices (for direct array indexing) we allow for variants of the kaomojis on the
    servers.

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,5 +1,7 @@
 import {
+  inlineCode,
   SlashCommandBuilder,
+  SlashCommandIntegerOption,
   SlashCommandStringOption,
 } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
@@ -16,16 +18,20 @@ import {
 import glob from 'glob';
 import { promisify } from 'util';
 import Event from '../interfaces/EventStorage';
+import * as file from '../../data/kaomojis.json';
+import Kaomoji from '../types/Kaomoji';
 
 const globPromise = promisify(glob);
+
+const MAXIMUM_OPTIONS = 25;
 
 export default class Bot extends Client {
   // readonly members
   public readonly events: Collection<string, Event> = new Collection();
-  public readonly kaomojis: Collection<string, string>;
+  public readonly kaomojis: Collection<string, string | readonly string[]>;
   public readonly logger: Consola = consola;
 
-  public constructor(kaomojis: Collection<string, string>) {
+  public constructor() {
     super({
       // only need access to send and delete messages inside guild
       intents: [Intents.FLAGS.GUILD_MESSAGES],
@@ -52,7 +58,10 @@ export default class Bot extends Client {
       .catch((e: unknown) => this.logger.error(e));
 
     // load Kaomojis
-    this.kaomojis = kaomojis || new Collection<string, string>();
+    this.kaomojis = new Collection<string, string | readonly string[]>();
+    (file.kaomojis as Kaomoji[]).forEach((kaomoji: Kaomoji) =>
+      this.kaomojis.set(kaomoji.name, kaomoji.data),
+    );
   }
 
   public start(): void {
@@ -72,20 +81,55 @@ export default class Bot extends Client {
     // register kaomoji slash commands
     new REST({ version: '9' })
       .setToken(process.env.DISCORD_TOKEN as string)
-      .put(Routes.applicationCommands(process.env.CLIENT_ID as string), {
-        body: Array.from(
-          this.kaomojis.entries(),
-          ([name, data]: [string, string]) =>
-            new SlashCommandBuilder()
-              .setName(name)
-              .setDescription(`Appends ${data} to your message.`)
-              .setDefaultPermission(true)
-              .addStringOption((option: SlashCommandStringOption) =>
-                option.setName('message').setDescription('Your message'),
-              )
-              .toJSON(),
+      .put(
+        Routes.applicationGuildCommands(
+          process.env.CLIENT_ID as string,
+          process.env.GUILD_ID as string,
         ),
-      })
+        {
+          body: [...this.kaomojis.entries()].flatMap<
+            ReturnType<SlashCommandBuilder['toJSON']>
+          >(([name, data]: [string, string | readonly string[]]) =>
+            Array.isArray(data) && data.length > 1
+              ? // variant case
+                new SlashCommandBuilder()
+                  .setName(name)
+                  .setDescription(
+                    `Appends ${data[0]} or a variant to your message.`,
+                  )
+                  .setDefaultPermission(true)
+                  .addIntegerOption((option: SlashCommandIntegerOption) =>
+                    option
+                      .setName('variant')
+                      .setDescription(
+                        `Desired variant of ${inlineCode(name)} kaomoji`,
+                      )
+                      .addChoices(
+                        data
+                          // slash commands can only have 25 options
+                          .slice(0, MAXIMUM_OPTIONS)
+                          .map((datum: string, index: number) => [
+                            datum,
+                            index,
+                          ]),
+                      ),
+                  )
+                  .addStringOption((option: SlashCommandStringOption) =>
+                    option.setName('message').setDescription('Your message'),
+                  )
+                  .toJSON()
+              : // no variant case
+                new SlashCommandBuilder()
+                  .setName(name)
+                  .setDescription(`Appends ${data} to your message.`)
+                  .setDefaultPermission(true)
+                  .addStringOption((option: SlashCommandStringOption) =>
+                    option.setName('message').setDescription('Your message'),
+                  )
+                  .toJSON(),
+          ),
+        },
+      )
       .then(() =>
         this.logger.success(
           'Successfully registered all application commands!',
