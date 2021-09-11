3891d7b9ff0d97b23d61e019f1a79b173cf925c5
Mahyar Mirrashed <mah.mirr@gmail.com>
2021-09-11 16:09:54 -0500

    feat: ensure needed environment variables are set

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,14 @@ import Bot from './client/Client';
 // set up environment variables from .env file
 config();
 
-new Bot().start();
+// ensure environment variables are set
+if (
+  [process.env.CLIENT_ID, process.env.DISCORD_TOKEN].every(
+    (envVar: string | undefined) => envVar !== undefined,
+  )
+) {
+  new Bot().start();
+} else {
+  // eslint-disable-next-line no-console
+  console.error('One or more environment variables are not set.');
+}
